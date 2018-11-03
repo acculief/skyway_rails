6192ce9f25914d14f240c8fa6ba9f881674e6e91
acculief <yajousaki@gmail.com>
2018-11-03 23:56:27 +0900

    user登録を別で作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only:[:index, :edit, :update]
   # ユーザー新規登録時（フォームでユーザー情報と住所を入力するところ）
   def index
   end
-
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in(@user)
+      redirect_to home_path
+    else
+      render 'new'
+    end
+  end
   def edit
     @user = current_user
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'home', to:'home#index'
   get 'maps', to:'maps#index'
   get 'users', to:'users#index'
+  get 'users/new', to:'users#new'
+  post 'users/create', to:'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
